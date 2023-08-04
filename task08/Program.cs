c5cd7789bb094c4e5e7f71bb510ee5c2ceb5f960
Igor Shtengelov <kontarkovi@yandex.com>
2023-08-04 20:16:09 +0500

    Семинар 1. ДЗ. task08

---
+++ task08/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(num + " -> ");
+for (int i = 2; i <= num; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        if ((i < num) && (i + 1 != num)) Console.Write(", ");
+    }
+}
+Console.WriteLine();
