0de49c6f547d2a109ca418e77232f4c43cd3b26b
Igor Shtengelov <kontarkovi@yandex.com>
2023-08-17 22:41:10 +0500

    Семинар 2. task14

---
+++ task14/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+14 -> нет
+46 -> нет
+161 -> да
+*/
+
+string message = "нет";
+int numberOne = 7;
+int numberTwo = 23;
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % (numberOne * numberTwo) == 0) message = "да";
+Console.WriteLine($"{number} -> {message}");
